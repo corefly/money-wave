3accff53d5a6d958047db754374f75896677327d
corefly <jukandrey208@gmail.com>
2024-07-14 21:39:38 +0300

    Setup identity

---
+++ Services/Identity/Identity.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Identity.Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Api.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : IdentityDbContext<ApplicationUser>(options);
